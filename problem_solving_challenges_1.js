3147ebc21dc732b2203066065b4175da45575623
calvin <calvin.francis96@outlook.com>
2021-12-01 12:40:50 -0600

    Solve multiple pointers challenge

--- problem_solving_challenges_1.js
+++ problem_solving_challenges_1.js
@@ -12,9 +12,14 @@ function sameFrequency(num1, num2){
         //if the character is not a key in frequencies or the value of the key is zero, return false.
     // reutrn true
 
+    //Solve:
     const str1 = `${num1}`
     const str2 = `${num2}`
+    if (str1.length !== str2.length) {
+        return false
+    }
     const frequencies = {}
+
 
     for(let char of str1) {
         frequencies[char] = (frequencies[char] || 0) + 1;
@@ -28,7 +33,62 @@ function sameFrequency(num1, num2){
         }
     }
     return true
-  }
+}
 
-  console.log(sameFrequency(182, 218))
-  console.log(sameFrequency(1,12))
+function areThereDuplicatesFC() {
+    //Problem: Write a function that accepts a variable number of arguments(integers). The function should return true if there
+    //are duplicates passed in and false otherwise.
+
+    //Example: areThereDuplicates(1, 2, 3) // false. areThereDuplicates(1, 1, 2) // true.
+
+    //Breakdown:
+    // Step 1: The function does not need parameters since it takes a variable number of arguments. Use the arguments object
+    // to collect the values passed in.
+    // Step 2: Create an empty object called frequencies to collect the frequency of each element in the arguments array.
+    // Step 3: Iterate through frequencies and check that that value of each key is one. If so, return true. Otherwise,
+    // return false.
+
+    //Solve:
+    const frequencies = {};
+
+    for(let i = 0; i < arguments.length; i++) {
+        frequencies[arguments[i]] = (frequencies[arguments[i]] || 0) + 1;
+    }
+
+    for(let key in frequencies) {
+        if(frequencies[key] !== 1) {
+            return true
+        }
+    }
+    return false
+}
+
+function areThereDuplicatesMP(...args) {
+    //Problem: Write a function that accepts a variable number of arguments(integers). The function should return true if there
+    //are duplicates passed in and false otherwise.
+
+    //Example: areThereDuplicates(1, 2, 3) // false. areThereDuplicates(1, 1, 2) // true.
+
+    //Breakdown:
+    // Step 1: The function does not need parameters since it takes a variable number of arguments. Use the arguments object
+    // to collect the values passed in. Using ...args as a parameter gives access to the arguments as an array.
+    // Step 2: Create two pointers. Set the first eqaul to zero and the second equal to one.
+    // Step 3: Sort the arguments array.
+    // Step 4: Loop through the arguments array. If the first pointer equals the second, return true. Otherwise, increment each
+    // pointer by one.
+
+    //Solve:
+    let pointer1 = 0;
+    args.sort();
+
+    for(let pointer2 = 1; pointer2 < args.length; pointer2++) {
+        if(args[pointer1] === args[pointer2]) {
+            return true
+        } else {
+            pointer1++
+        }
+    }
+    return false
+}
+
+console.log(areThereDuplicatesMP(1,3,1))
