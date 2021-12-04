2f0b4425f3725a726b77051cfe2c0d8617719bc9
calvin <calvin.francis96@outlook.com>
2021-12-04 12:37:42 -0600

    Add solution to longest substring problem

--- problem_solving_challenges_1.js
+++ problem_solving_challenges_1.js
@@ -242,3 +242,22 @@ function minSubArrayLen(array, target){
     }
     return minimumLength === array.length ? 0 : minimumLength
 }
+
+// Couldn't Solve - Come back later:
+function findLongestSubstring(str) {
+    let longest = 0;
+    let seen = {};
+    let start = 0;
+   
+    for (let i = 0; i < str.length; i++) {
+      let char = str[i];
+      if (seen[char]) {
+        start = Math.max(start, seen[char]);
+      }
+      // index - beginning of substring + 1 (to include current in count)
+      longest = Math.max(longest, i - start + 1);
+      // store the index of the next char so as to not double count
+      seen[char] = i + 1;
+    }
+    return longest;
+  }
