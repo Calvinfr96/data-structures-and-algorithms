d198d2750849beeac8e940204707694f727391c1
calvin <calvin.francis96@outlook.com>
2021-12-04 23:33:04 -0600

    Refactor solution to power recursion problem

--- recursion_challenges.js
+++ recursion_challenges.js
@@ -37,4 +37,34 @@ function power(base, exponent){
     return product;
 }
 
-console.log(power(81, 0))
+function powerRF(base, exponent){
+    //Problem: Write a function that, given a base and an exponent, returns the result of raising the base to the exponent.
+    //Do not worry about negative exponents or bases.
+
+    //Examples: power(2,2) // 4. power(2,0) // 1. power(2,4) // 16.
+
+    //Breakdown:
+    // Step 1: If the exponent is zero, return 1.
+    // Step 2: Define a variable called product and initialize it as the base.
+    // Step 3: Create a recursive function called multiply. Within multiply:
+    // Step 3a: If the exponent is zero, return 1. If the exponent is 1, return the product. Otherwise, call multiply recursively and
+    // pass it the base and (exponent - 1) as arguments.
+    // Step 3b: Return the updated product.
+    // Step 4: Call multiply by passing it the base and the product.
+    // Step 5: return the product.s
+
+    //Solve:
+    let product  = base;
+
+    if(exponent === 0) {
+        return 1;
+    } else if(exponent === 1) {
+        return product;
+    } else {
+        product*= powerRF(base, exponent-1)
+    }
+
+    return product;
+}
+
+console.log(powerRF(2, 4))
