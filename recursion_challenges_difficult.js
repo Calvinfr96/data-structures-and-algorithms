2abe174a70ba0d29b15227f75b62f3acb2c6b2e6
calvin <calvin.francis96@outlook.com>
2021-12-06 11:11:15 -0600

    Solve more recursion problems

--- recursion_challenges_difficult.js
+++ recursion_challenges_difficult.js
@@ -30,3 +30,82 @@ function isPalindrome(string) {
     //Solve:
     return string === reverse(string) ? true : false
 }
+
+function someRecursive(array, callback) {
+    //Problem: Write a function that accepts an array and a callback and returns true if a single value in the array returns true when passed
+    //to the callback, and false otherwise.
+
+    //Examples: const isOdd = val => val % 2 !== 0; someRecursive([1,2,3,4], isOdd) // true. someRecursive([2,4,6,8], isOdd) // false.
+
+    //Breakdown:
+    // Step 1: Define a variable called elementValid and initialize it to false.
+    // Step 2: If the array is empty, return false.
+    // Step 3: If the callback returns true when the first element of the array is passed to it, return true. Otherwise, call someRecursive
+    // by passing it array.slice(1) and the callback.
+
+    //Solve:
+
+    if(array.length === 0) {
+        return false;
+    } else if(callback(array[0])) {
+        return true;
+    } else {
+        return someRecursive(array.slice(1), callback)
+    }
+}
+
+function flatten(array) {
+    //Problem: Write a function that transforms a multidimensional array into a one dimensional array.
+
+    //Examples: flatten([1,2,3,[4,5]]) // [1,2,3,4,5]. flatten(1,2,[3,[4,5,[6,7]]]) // [1,2,3,4,5,6,7].
+
+    //Breakdown:
+    // Step 1: Define a variable called flattenedArray and initialize it as an empty array.
+    // Step 2: Iterate through the array using a for...of loop, for each element: 
+    // Step 2a: If the element is an array, call flatten on that element.
+    // Step 2b: If the element is not an array, push it onto flattenedArray.
+    // Step 3: Retrun flattenedArray
+
+    //Solve:
+    let flattenedArray = [];
+
+    function collect(array) {
+        for(let element of array) {
+            if(Array.isArray(element)) {
+                collect(element);
+            } else {
+                flattenedArray.push(element);
+            }
+        }
+    }
+
+    collect(array);
+    return flattenedArray;
+}
+
+function flattenPR(array) {
+    //Problem: Write a function that transforms a multidimensional array into a one dimensional array.
+
+    //Examples: flatten([1,2,3,[4,5]]) // [1,2,3,4,5]. flatten(1,2,[3,[4,5,[6,7]]]) // [1,2,3,4,5,6,7].
+
+    //Breakdown:
+    // Step 1: Define a variable called flattenedArray and initialize it as an empty array.
+    // Step 2: If the input array length is zero, return flattenedArray.
+    // Step 3: If the first element of array is an array, call flatten on that element.
+    // Step 4: If the first element of the array is not an array, push it onto flattenedArray.
+    // Step 5: Update the value of flattenedArray by concatenating it with the flatten(array.slice(1)).
+
+    //Solve:
+    let flattenedArray = [];
+
+    if(array.length === 0) {
+        return flattenedArray;
+    }
+    
+    if(!Array.isArray(array[0])) {
+        flattenedArray.push(array[0]);
+    }
+
+    flattenedArray = flattenedArray.concat(flatten(array.slice(1)));
+    return flattenedArray;
+}
