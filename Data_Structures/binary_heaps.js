a2bde90602dccc7da7dc3297bd7c8b5e687e1704
calvin <calvin.francis96@outlook.com>
2021-12-28 13:27:51 -0600

    Add notes on removing a node from a binary heap

--- Data_Structures/binary_heaps.js
+++ Data_Structures/binary_heaps.js
@@ -1,3 +1,7 @@
+function swap(array,a,b) {
+    [array[a], array[b]] = [array[b], array[a]];
+}
+
 class MaxBinaryHeap {
     constructor() {
         this.values = [];
@@ -10,10 +14,6 @@ class MaxBinaryHeap {
         let childValue = this.values[childIndex];
         let parentValue = this.values[parentIndex];
 
-        function swap(array,a,b) {
-            [array[a], array[b]] = [array[b], array[a]];
-        }
-
         while(childIndex > 0 && childValue > parentValue) {
             swap(this.values, parentIndex, childIndex);
             childIndex = parentIndex;
@@ -22,10 +22,43 @@ class MaxBinaryHeap {
             parentValue = this.values[parentIndex];
         }
     }
+
+    remove() {
+        function greaterIndex(array, a, b) {
+            const length = array.length
+            if(a < length && b < length) {
+                const maxValue = Math.max(array[a], array[b]);
+                let maxIndex;
+                array[a] === maxValue ? maxIndex = a : maxIndex = b;
+                return maxIndex;
+            } else {
+                return a < length ? a : b
+            }
+        }
+
+        if(!this.values.length) {
+            return undefined;
+        } else if(this.values.length === 1) {
+            return this.values.pop();
+        } else {
+            swap(this.values, 0, this.values.length - 1);
+            const head = this.values.pop();
+            let currentIndex = 0;
+            let currentValue = this.values[currentIndex];
+            let childIndex = greaterIndex(this.values, 2*currentIndex + 1, 2*currentIndex + 2);
+            let childValue = this.values[childIndex];
+
+            while(childValue > currentValue) {
+                swap(this.values, currentIndex, childIndex);
+                currentIndex = childIndex;
+                childIndex = greaterIndex(this.values, 2*currentIndex + 1,2*currentIndex + 2);
+                currentValue = this.values[currentIndex];
+                childValue = this.values[childIndex];
+            }
+
+            return head;
+        }
+    }
 }
 
 const heap = new MaxBinaryHeap();
-heap.values = [];
-console.log(heap)
-heap.insert(34)
-console.log(heap)
