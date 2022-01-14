b6b158c6221d14922dfc404ada2f381968a22563
calvin <calvin.francis96@outlook.com>
2022-01-14 17:46:08 -0600

    Update dijkstra's method

--- Data_Structures/dijkstras_algorithm.js
+++ Data_Structures/dijkstras_algorithm.js
@@ -49,45 +49,44 @@ class WeightedGraph {
     }
 
     dijkstras(start, end) {
-        const nodes = new PriorityQueue(); //Creates priority queue that we dequeue from for each iteration of the loop.
-        const distances = {}; //Stores the distances from the starting node.
-        const previous = {}; // Stores the previous node in the traversal to the given node.
-        const path = []; //Stores the traversal path to be returned at the end.
-        let smallest; //Stores the node with the shortest distance to the starting node.
+        const distances = {};
+        const previous = {};
+        const nodes = new PriorityQueue();
+        const path = [];
+        let smallest;
 
         for(let vertex in this.adjacencyList) {
-            if(vertex === start) { //Ensures the starting node has a distance of zero and a priority of zero (highest).
-                distances[vertex] = 0;
+            if(vertex === start) {
                 nodes.insert(vertex, 0);
-            } else { //Ensures all other nodes have a distance of infinity (unknown) and a priorty of infinity (lowest).
-                distances[vertex] = Infinity;
+                distances[vertex] = 0;
+            } else {
                 nodes.insert(vertex, Infinity);
+                distances[vertex] = Infinity;
             }
-            previous[vertex] = null; //At the start, the graph hasn't been traversed, so there is no previous node in the traversal to a given node.
+            previous[vertex] = null;
         }
 
-        while(nodes.values.length) { //While there are nodes in the queue...
-            smallest = nodes.remove().value; //Sets smallest to the node with the smallest distance from the starting node in the queue.
-            if(smallest === end) { //When the smallest node in the queue is the ending node, the travseral is complete.
-                while(previous[smallest]) { //Builds path array to be returned when the traversal is complete.
-                    path.push(smallest);
+        while(nodes.values.length) {
+            smallest = nodes.remove().value;
+            if(smallest === end) {
+                path.unshift(smallest);
+                while(previous[smallest]) {
+                    path.unshift(previous[smallest]);
                     smallest = previous[smallest];
                 }
                 break;
             }
-            if(smallest || distances[smallest] !== Infinity) { //As long as the vertex dequeued is valid...
-                for(let neighbor in this.adjacencyList[smallest]) { //For each node in the adjacency list of 'smallest'.
-                    let nextNode = this.adjacencyList[smallest][neighbor]; //The node in the adjacency list (can be improved with for... of).
-                    let candidate = distances[smallest] + nextNode.weight; //Potentially smaller distance from starting node.
-                    if(candidate < distances[nextNode.node]) { //If the candidate is smaller than the distance currently stored in 'distances'...
-                        distances[nextNode.node] = candidate; //Update the distance for that node to be the smaller distance.
-                        previous[nextNode.node] = smallest; //Update the previous for the node to be 'smallest'.
-                        nodes.insert(nextNode.node, candidate); //Add the neighbor of 'smallest to the queue with the updated priority.
-                    }
+
+            for(let neighbor of this.adjacencyList[smallest]) {
+                const candidate = neighbor.weight + distances[smallest];
+                if(candidate < distances[neighbor.node]) {
+                    distances[neighbor.node] = candidate;
+                    previous[neighbor.node] = smallest;
+                    nodes.insert(neighbor.node, candidate);
                 }
-            }
+            }  
         }
-        return path.concat(smallest).reverse(); //Add the starting node to the path and reverse it (it was built backwards in the loop).
+        return path;
     }
 }
 
@@ -196,7 +195,7 @@ graph2.addEdge("C", "F", 4);
 graph2.addEdge("D", "E", 3);
 graph2.addEdge("D", "F", 1);
 graph2.addEdge("E", "F", 1);
-console.log(graph2.dijkstras("A", "E"));
+graph2.dijkstras("C", "F");
 
 // Practice:
 const distances = {
