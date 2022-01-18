c2275e247b19702898f4ff41e4b400858738eea9
calvin <calvin.francis96@outlook.com>
2022-01-18 17:54:52 -0600

    Practice sorting problem

--- Coding_Exercises/sorting_exercises.js
+++ Coding_Exercises/sorting_exercises.js
@@ -102,4 +102,42 @@ function selectionSort2(array, comparator) {
 }
 
 const array2 = [0, -10, 7, 4];
-console.log(selectionSort2(array2))
+
+function insertionSort(array) {
+    for(let i = 1; i < array.length; i++) {
+        const element = array[i];
+        for(let j = i - 1; j >= 0; j--) {
+            if(element < array[j]) {
+                [array[j], array[j+1]] = [array[j+1], array[j]];
+            } else break
+        }
+    }
+    return array
+}
+
+function insertionSort2(array, comparator) {
+    if(typeof comparator !== 'function') {
+        comparator = function(a,b) {
+            if(a < b) {
+                return 1;
+            } else if(a > b) {
+                return -1;
+            } else {
+                return 0;
+            }
+        }
+    }
+
+    for(let i = 1; i < array.length; i++) {
+        const element = array[i];
+        for(let j = i - 1; j >= 0; j--) {
+            if(comparator(element, array[j] > 0)) {
+                [array[j], array[j+1]] = [array[j+1], array[j]];
+            } else break
+        }
+    }
+    return array
+}
+
+const array3 = [9, 10, 12, 20, 4, 4, 7];
+console.log(insertionSort(array3));
