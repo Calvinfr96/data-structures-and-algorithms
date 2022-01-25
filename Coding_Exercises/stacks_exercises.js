aea20e934ae81ee711531bd2989431565bfead2b
calvin <calvin.francis96@outlook.com>
2022-01-25 09:36:50 -0600

    Create queue class for implementing a stack using two queues

--- Coding_Exercises/stacks_exercises.js
+++ Coding_Exercises/stacks_exercises.js
@@ -46,13 +46,43 @@ class Stack {
     }
 }
 
-const stack = new Stack;
-console.log(stack)
-stack.push("One")
-console.log(stack)
-stack.push("Two")
-console.log(stack)
-stack.push("Three")
-console.log(stack)
-console.log(stack.pop())
-console.log(stack)
+//Implementing a Stack With Two Queues
+class Queue {
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+
+    enqueue(value) {
+        const node = new Node(value);
+
+        if(!this.size) {
+            this.first = node;
+            this.last = this.first;
+        } else {
+            this.last.next = node;
+            this.last = this.last.next;
+        }
+
+        this.size++;
+        return this.size;
+    }
+
+    dequeue() {
+        if(!this.size) {
+            return undefined;
+        }
+
+        const first = this.first;
+        if(this.first === this.last) {
+            this.first = null;
+            this.last = null;
+        } else {
+            this.first = this.first.next;
+        }
+
+        this.size--;
+        return first;
+    }
+}
