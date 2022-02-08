2c6033a7b1d2ebf9aadbc69fbe08281f6397be3d
calvin <calvin.francis96@outlook.com>
2022-02-08 09:29:38 -0600

    Solve binary heap 'extract max' problem

--- Coding_Exercises/binary_heap_exercises.js
+++ Coding_Exercises/binary_heap_exercises.js
@@ -22,12 +22,48 @@ class MaxBinaryHeap {
             parentValue = this.values[parentIndex];
         }
     }
+
+    greaterChild(parentIndex) {
+        const childIndex1 = 2*parentIndex + 1;
+        const childIndex2 = 2*parentIndex + 2;
+        const childValue1 = this.values[childIndex1];
+        const childValue2 = this.values[childIndex2];
+
+        if(childValue1 && childValue1 > childValue2) {
+            return childIndex1;
+        } else if(childValue2 && childValue2 > childValue1) {
+            return childIndex2;
+        } else {
+            return undefined;
+        }
+    }
+
+    extractMax() {
+        if(!this.values.length) {
+            return undefined;
+        }
+
+        this.swap(this.values, 0, this.values.length - 1);
+        const max = this.values.pop();
+        let parentIndex = 0;
+        let childIndex = this.greaterChild(parentIndex);
+
+        while(childIndex) {
+            this.swap(this.values, parentIndex, childIndex);
+            parentIndex = childIndex;
+            childIndex = this.greaterChild(parentIndex)
+        }
+
+        return max;
+    }
 }
 
 const heap = new MaxBinaryHeap();
-heap.insert(1)
-heap.insert(2)
-heap.insert(3)
-heap.insert(4)
-heap.insert(5)
+heap.insert(10)
+heap.insert(9)
+heap.insert(8)
+heap.insert(7)
 heap.insert(6)
+heap.insert(5)
+heap.insert(4)
+heap.insert(20)
