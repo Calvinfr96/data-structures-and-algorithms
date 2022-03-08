a3e4e8905bac79d602390b430e7bae46cb226197
calvin <calvin.francis96@outlook.com>
2022-03-08 10:15:16 -0600

    Add notes on Tries

--- Coding_Exercises/trie_exercises.js
+++ Coding_Exercises/trie_exercises.js
@@ -4,6 +4,14 @@ class Trie {
         this.isWord = false; //This is an indication of whether the key forms a complete word.
     }
 
+    //Each character at the 'root' of the Trie represents all words that start with that letter.
+    //In the example below, we're adding 'mood' to the trie. So m is added to the 'root'.
+    //The isWord tells you if that character completes a word. No character at the 'root' should have isWord set to true.
+    //When the base case (the last letter of the word) is reached, the subTrie for that letter (with isWord set to true) is
+    //added as a value to the key that corresponds to the letter before it.
+    //So when 'd'is reached, isWord is set to true and that trie is set to the key 'o'.
+
+
     addWord(word, index = 0) {//Example: 'mood', 0, 1, 2, 3, 4
         if (index === word.length) {// false, false, false, false, true
             this.isWord = true;//
@@ -14,5 +22,9 @@ class Trie {
             this.characters[char] = subTrie;//
         }
         return this;//
+    }
+
+    removeWord(word, index = 0) {
+
     }
 }
