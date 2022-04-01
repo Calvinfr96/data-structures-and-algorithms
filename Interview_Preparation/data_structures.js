b647f3deb0d811e439800b1d2888a5ad973913a9
calvin <calvin.francis96@outlook.com>
2022-04-01 09:37:10 -0500

    Practice array and string interview questions

--- Interview_Preparation/data_structures.js
+++ Interview_Preparation/data_structures.js
@@ -25,6 +25,7 @@ function isUnique2(string) { //Optimal Time - Frequency Counters
     return true;
 }
 
+//Question 1.2
 function checkPermutation(string1, string2) {
     if(string1.length !== string2.length) {
         return false;
@@ -69,6 +70,7 @@ function checkPermutation2(string1, string2) {
     return true;
 }
 
+//Question 1.3
 function URLify(string) {
     charArray = [];
 
@@ -85,6 +87,7 @@ function URLify(string) {
     return charArray
 }
 
+//Question 1.4
 function paildromePermutation(string) {
     //Palindrome: A word that is spelled the same forwards and backwards
     //Basically, check if the word could be a palindrome if you rearranged the letters.
@@ -116,10 +119,77 @@ function paildromePermutation(string) {
 
 }
 
-function editedOnce(string1, string2) {
-    if(Math.abs(string1.length - string2.length > 1)) {
+//Question 1.5
+function checkOneInsertionOrDeletion(string1, string2) {
+    if(Math.abs(string1.length - string2.length) > 1) {
         return false;
     }
+
+    const shorter = string1.length < string2.length ? string1 : string2;
+    const longer = string1.length < string2.length ? string2 : string1;
+
+    let pointer1 = 0;
+    let pointer2 = 0;
+
+    while(pointer1 < longer.length && pointer2 < shorter.length) {
+        if(longer[pointer1] === shorter[pointer2]) {
+            pointer2++;
+        }
+        pointer1++;
+    }
+
+    return pointer2 === shorter.length;
+
 }
 
-console.log(editedOnce("poop", "pop"));
+function checkOneCharReplaced(string1, string2) {
+    if(string1.length !== string2.length) {
+        return false;
+    }
+    let mismatches = 0;
+
+    for(let i = 0; i < string1.length; i++) {
+        if(string1[i] !== string2[i]) {
+            mismatches++;
+        }
+    }
+
+    return mismatches < 2
+}
+
+function editedOnce(string1, string2) {
+    if(string1 === string2) {
+        return true;
+    }
+
+    const checkOneInsertedOrDeleted = checkOneInsertionOrDeletion(string1, string2);
+    const checkOneReplacement = checkOneCharReplaced(string1, string2);
+
+    if(checkOneInsertedOrDeleted && checkOneReplacement) {
+        return false;
+    }
+
+    return checkOneInsertedOrDeleted || checkOneReplacement;
+}
+
+//Question 1.6
+function stringCompression(string) {
+    let frequency = 1;
+    let result = "";
+
+    //aabcccccaaa
+
+    for(let i = 1; i <= string.length; i++) {
+        if(string[i] === string[i - 1]) {
+            frequency++;
+        } else {
+            result += string[i - 1];
+            result += frequency;
+            frequency = 1;
+        }
+    }
+
+    return result.length > string.length ? string : result;
+}
+
+console.log(stringCompression(""))
