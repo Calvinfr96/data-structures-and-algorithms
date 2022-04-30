c0a4a941db59c4fd0ff0a5a2b999afd23e163442
calvin <calvin.francis96@outlook.com>
2022-04-30 11:02:44 -0500

    Finish reviewing intermediate sorting algorithms

--- Coding_Exercises/Practice_Problems/src/IntermediateSorting.java
+++ Coding_Exercises/Practice_Problems/src/IntermediateSorting.java
@@ -1,13 +1,16 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 /*
 * The intermediate sorting algorithms (Merge, Quick, and Radix Sort) improve upon the basic sorting algorithms by lowering the time complexity of the sort. However, they are harder to understand.
 * Merge and Quick Sort take advantage of the fact that an array of size one is inherently sorted, while Radix sort takes advantage of a unique property of integers.
+* Merge and Quick Sort, along with the basic sorting algorithms are known as comparison sorts, which have a minimum time complexity of O(N*log(N)).
 * While Radix Sort is effecient, the algorithm used can't properly sort negative values.
 */
 public class IntermediateSorting {
+    //Merge Sort
     public static List<Integer> mergeSort(List<Integer> array) {
         return mergeSort(array, 0, array.size());
     }
@@ -24,20 +27,41 @@ public class IntermediateSorting {
 
     }
 
+    //Quick Sort
     public static List<Integer> quickSort(List<Integer> array) {
         return quickSort(array, 0, array.size() - 1);
     }
 
     private static List<Integer> quickSort(List<Integer> array, int start, int end) {
-        int pivotElement = pivotHelper(array, start, end);
-        if(start < end && pivotElement < array.size() - 1) {
-            quickSort(array, 0, pivotElement - 1);
-            quickSort(array, pivotElement + 1, end);
+        if(start < end) {
+            int pivotIndex = pivotHelper(array, start, end);
+            quickSort(array, 0, pivotIndex - 1);
+            quickSort(array, pivotIndex + 1, end);
         }
 
         return array;
     }
 
+    //Radix Sort
+    public static List<Integer> radixSort(List<Integer> array) {
+        int maxDigits = maxDigits(array);
+
+        for(int i = 0; i < maxDigits; i++) {
+            List<List<Integer>> buckets = new ArrayList<>(10);
+            for(int j = 0; j < 10; j++) {
+                buckets.add(new ArrayList<>());
+            }
+            for(Integer num : array) {
+                int position = findDigit(num, i);
+                buckets.get(position).add(num);
+            }
+
+            array = flatten(buckets);
+        }
+        return array;
+    }
+
+    //Merge Sort Helper Method
     private static List<Integer> mergeHelper(List<Integer> list1, List<Integer> list2) {
         List<Integer> combinedList = new ArrayList<>();
         int pointer1 = 0;
@@ -65,7 +89,8 @@ public class IntermediateSorting {
         return combinedList;
     }
 
-    public static int pivotHelper(List<Integer> array, int start, int end) {
+    //Quick Sort Helper Method
+    private static int pivotHelper(List<Integer> array, int start, int end) {
         int pivotElement = array.get(start);
         int pivotIndex = start;
 
@@ -79,6 +104,34 @@ public class IntermediateSorting {
         return pivotIndex;
     }
 
+    //Radix Sort Helper Methods
+    private static int findDigit(int num, int index) {
+        return (int) Math.floor(Math.abs(num) / Math.pow(10, index)) % 10;
+    }
+
+    private static int numDigits(int num) {
+        return (int) Math.floor(Math.log10(num)) + 1;
+    }
+
+    private static int maxDigits(List<Integer> nums) {
+        Collections.sort(nums);
+        Collections.reverse(nums);
+        
+        return numDigits(nums.get(0));
+    } 
+
+    private static List<Integer> flatten(List<List<Integer>> array) {
+        List<Integer> flattened = new ArrayList<>();
+        for(List<Integer> list : array) {
+            for(Integer num : list) {
+                flattened.add(num);
+            }
+        }
+
+        return flattened;
+    }
+
+    //Swapping Helper Method
     private static List<Integer> swap(List<Integer> array, int a, int b) {
         if(array.size() < 2) {
             return array;
@@ -91,7 +144,7 @@ public class IntermediateSorting {
         return array;
     }
     public static void main(String[] args) {
-        List<Integer> list = Arrays.asList(5,4,3,2,1);
-        System.out.println(quickSort(list));
+        List<Integer> nums = Arrays.asList(1, 10, 5, 10000, 500, 1000, 2, 30, 4);
+        System.out.println(radixSort(nums));
     }
 }
