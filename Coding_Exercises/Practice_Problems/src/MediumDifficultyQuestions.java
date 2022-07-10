8020ef9a8779b13f300c4fe8e91f56ace7bf57f3
calvin <calvin.francis96@outlook.com>
2022-07-10 10:13:15 -0500

    Solve 'min heap construction' interview question

--- Coding_Exercises/Practice_Problems/src/MediumDifficultyQuestions.java
+++ Coding_Exercises/Practice_Problems/src/MediumDifficultyQuestions.java
@@ -1689,4 +1689,78 @@ public class MediumDifficultyQuestions {
       //Time: O(n) where n is the number of cities
       //Space: O(1)
     }
+
+    static class MinHeap {
+      List<Integer> heap = new ArrayList<Integer>();
+  
+      public MinHeap(List<Integer> array) {
+        heap = buildHeap(array);
+      }
+  
+      public List<Integer> buildHeap(List<Integer> array) {
+        // Write your code here.
+        int firstParentIndex = (array.size() - 2) / 2;
+        for(int currentIndex = firstParentIndex; currentIndex >= 0; currentIndex--) {
+          siftDown(currentIndex, array.size() - 1, array);
+        }
+        return array;
+      }
+  
+      public void siftDown(int currentIdx, int endIdx, List<Integer> heap) {
+        // Write your code here.
+        int childOneIdx = 2*currentIdx + 1;
+        while (childOneIdx <= endIdx) {
+          int childTwoIdx = 2*currentIdx + 2 <= endIdx ? 2*currentIdx + 2 : -1;
+          int idxToSwap;
+          if(childTwoIdx != -1 && heap.get(childTwoIdx) < heap.get(childOneIdx)) {
+            idxToSwap = childTwoIdx;
+          } else {
+            idxToSwap = childOneIdx;
+          }
+          if(heap.get(idxToSwap) < heap.get(currentIdx)) {
+            swap(currentIdx, idxToSwap, heap);
+            currentIdx = idxToSwap;
+            childOneIdx = currentIdx * 2 + 1;
+          } else {
+            return;
+          }
+        }
+      }
+  
+      public void siftUp(int currentIdx, List<Integer> heap) {
+        // Write your code here.
+        int parentIdx = (currentIdx - 1) / 2;
+        while(currentIdx > 0 && heap.get(currentIdx) < heap.get(parentIdx)) {
+          swap(currentIdx, parentIdx, heap);
+          currentIdx = parentIdx;
+          parentIdx = (currentIdx - 1) / 2;
+        }
+      }
+  
+      public int peek() {
+        // Write your code here.
+        return heap.get(0);
+      }
+  
+      public int remove() {
+        // Write your code here.
+        swap(0, heap.size() - 1, heap);
+        int valueToRemove = heap.get(heap.size() - 1);
+        heap.remove(heap.size() - 1);
+        siftDown(0, heap.size() - 1, heap);
+        return valueToRemove;
+      }
+  
+      public void insert(int value) {
+        // Write your code here.
+        heap.add(value);
+        siftUp(heap.size() - 1, heap);
+      }
+  
+      public void swap(int i, int j, List<Integer> heap) {
+        Integer temp = heap.get(j);
+        heap.set(j, heap.get(i));
+        heap.set(i, temp);
+      }
+    }
 }
