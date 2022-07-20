e794ff36cb27bb13f74b50658349fe4564862ae6
calvin <calvin.francis96@outlook.com>
2022-07-20 09:06:40 -0500

    Solve 'powerset' interview question using simpler algorithm

--- Coding_Exercises/Practice_Problems/src/MediumDifficultyQuestions.java
+++ Coding_Exercises/Practice_Problems/src/MediumDifficultyQuestions.java
@@ -2197,4 +2197,23 @@ public class MediumDifficultyQuestions {
         }
       }
     }
+
+    class Program {
+      public static List<List<Integer>> powerset(List<Integer> array) {
+        // Write your code here.
+        List<List<Integer>> subsets = new ArrayList<>();
+        subsets.add(new ArrayList<>());
+    
+        for(Integer element : array) {
+          int length = subsets.size();
+          for(int i = 0; i < length; i++) {
+            List<Integer> currentSubset = new ArrayList<>(subsets.get(i));
+            currentSubset.add(element);
+            subsets.add(currentSubset);
+          }
+        }
+        
+        return subsets;
+      }
+    }
 }
