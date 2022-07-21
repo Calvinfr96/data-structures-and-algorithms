f345e4c7768027a6188cb92b9d804b3c60c4ad25
calvin <calvin.francis96@outlook.com>
2022-07-20 21:32:43 -0500

    Solve 'phone number mnemonics' interview question

--- Coding_Exercises/Practice_Problems/src/MediumDifficultyQuestions.java
+++ Coding_Exercises/Practice_Problems/src/MediumDifficultyQuestions.java
@@ -2198,7 +2198,7 @@ public class MediumDifficultyQuestions {
       }
     }
 
-    class Program {
+    class PowersetProgram2 {
       public static List<List<Integer>> powerset(List<Integer> array) {
         // Write your code here.
         List<List<Integer>> subsets = new ArrayList<>();
@@ -2214,6 +2214,48 @@ public class MediumDifficultyQuestions {
         }
         
         return subsets;
+      }
+    }
+
+    class PhoneNumberMnemonicsProgram {
+      private static final Map<Character, String[]> DIGIT_LETTERS = new HashMap<>();
+      
+      static {
+        DIGIT_LETTERS.put('0', new String[] {"0"});
+        DIGIT_LETTERS.put('1', new String[] {"1"});
+        DIGIT_LETTERS.put('2', new String[] {"a", "b", "c"});
+        DIGIT_LETTERS.put('3', new String[] {"d", "e", "f"});
+        DIGIT_LETTERS.put('4', new String[] {"g", "h", "i"});
+        DIGIT_LETTERS.put('5', new String[] {"j", "k", "l"});
+        DIGIT_LETTERS.put('6', new String[] {"m", "n", "o"});
+        DIGIT_LETTERS.put('7', new String[] {"p", "q", "r", "s"});
+        DIGIT_LETTERS.put('8', new String[] {"t", "u", "v"});
+        DIGIT_LETTERS.put('9', new String[] {"w", "x", "y", "z"});
+      }
+      
+      public ArrayList<String> phoneNumberMnemonics(String phoneNumber) {
+        // Write your code here.
+        String[] currentMnemonic = new String[phoneNumber.length()];
+        Arrays.fill(currentMnemonic, "0");
+    
+        ArrayList<String> mnemonicsFound = new ArrayList<>();
+        mnemonicsHelper(0, phoneNumber, currentMnemonic, mnemonicsFound);
+        return mnemonicsFound;
+      }
+    
+      private void mnemonicsHelper(int idx, String phoneNumber, String[] currentMnemonic,
+                                   ArrayList<String> mnemonicsFound) {
+        if(idx == phoneNumber.length()) {
+          String mnemonic = String.join("", currentMnemonic);
+          mnemonicsFound.add(mnemonic);
+        } else {
+          char digit = phoneNumber.charAt(idx);
+          String[] letters = DIGIT_LETTERS.get(digit);
+          for(String letter : letters) {
+            currentMnemonic[idx] = letter;
+            mnemonicsHelper(idx + 1, phoneNumber, currentMnemonic, mnemonicsFound);
+          }
+        }
       }
     }
 }
